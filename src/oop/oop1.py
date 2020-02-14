7d94a162106e3c11ced9df1db6e9b8d630c4a70a
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-02-14 11:08:43 -0500

    completes oop1, tests passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,28 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Base class
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+# Subclasses of vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
